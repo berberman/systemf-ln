fad32362d3d88ba8e153f766aeeeb38776644999
Potato Hatsue <1793913507@qq.com>
2026-04-01 14:30:08 -0700

    Remove LcAt*

--- SystemF/Lc/Tm.lean
+++ SystemF/Lc/Tm.lean
@@ -16,18 +16,6 @@ inductive LcTm : Tm → Prop where
   | tApp t T : LcTm t → LcTy T → LcTm (t⦃T⦄)
   | tLam (L : Finset Name) t : (∀ X ∉ L, LcTm (t⟪$T X⟫)) → LcTm (Λ' t)
 
-/-
-  All bound *term* indices in `t` are strictly less than `k`.
-  Note: it ignores bound type variables, so `LcAtTm 0` is not equivalent to `LcTm`.
--/
-inductive LcAtTm : ℕ → Tm → Prop where
-  | bvar i k : i < k → LcAtTm k (#v i)
-  | fvar x k : LcAtTm k ($v x)
-  | app t₁ t₂ k : LcAtTm k t₁ → LcAtTm k t₂ → LcAtTm k (t₁ ◦ t₂)
-  | lam T t k : LcAtTm (k + 1) t → LcAtTm k (ƛ T => t)
-  | tApp t T k : LcAtTm k t → LcAtTm k (t⦃T⦄)
-  | tLam t k : LcAtTm k t → LcAtTm k (Λ' t)
-
 -- Example: `λ A => y 0` is a locally closed term
 example : LcTm (ƛ ($T "A") => $v "y" ◦ #v 0) := by
   apply LcTm.lam {"y"}
@@ -45,137 +33,43 @@ example : ¬ LcTm (ƛ ($T "A") => #v 1) := by
     specialize h₂ x hx
     cases h₂
 
-theorem lcAtTm_openTm_id {t : Tm} {k : ℕ} (h : LcAtTm k t) (n : ℕ) (hn : k ≤ n) (u : Tm) :
-    t⟪n, u⟫ = t := by
-  induction h generalizing n with
-  | bvar i k _ =>
-    simp only [openTm_bvar, beq_iff_eq, ite_eq_right_iff]
-    intro h
-    omega
-  | fvar x k => simp
-  | app t₁ t₂ k _ _ ih₁ ih₂ =>
-    simp only [openTm_app, Tm.app.injEq]
-    rw [ih₁ _ hn, ih₂ _ hn]
-    simp_all only [and_self]
-  | lam T t k _ ih =>
-    simp only [openTm_lam, Tm.lam.injEq, true_and]
-    rw [ih (n + 1) (by omega)]
-  | tApp t T k _ ih =>
-    simp only [openTm_tApp, Tm.tApp.injEq, and_true]
-    rw [ih _ hn]
-  | tLam t k _ ih =>
-    simp only [openTm_tLam, Tm.tLam.injEq]
-    rw [ih _ hn]
-
-theorem lcAtTm_of_openTm {t : Tm} {x : Name} {k : ℕ}
-    (h : LcAtTm k (t⟪k, $vx⟫)) : LcAtTm (k + 1) t := by
-  induction t generalizing k with
-  | bvar idx =>
-    simp only [openTm_bvar, beq_iff_eq] at h
-    split at h
-    · constructor
-      omega
-    · cases h
-      constructor
-      omega
-  | fvar name => constructor
-  | app t₁ t₂ t₁_ih t₂_ih =>
-    cases h
-    constructor
-    · apply t₁_ih
-      assumption
-    · apply t₂_ih
-      assumption
-  | tApp t T ih =>
-    cases h
-    constructor
-    · apply ih
-      assumption
-  | lam T t ih =>
-    cases h
-    constructor
-    apply ih
-    assumption
-  | tLam t ih =>
-    cases h
-    constructor
-    apply ih
-    assumption
-
-lemma lcAtTm_openTmTy_inv {t : Tm} {X : Name} {k n : ℕ}
-    (h : LcAtTm k (t⟪n, $TX⟫)) : LcAtTm k t := by
-  induction t generalizing k n with
+theorem openTm_neq_id {k j : ℕ} {t u v : Tm} (hNeq : k ≠ j) (h : t⟪j, v⟫⟪k, u⟫ = t⟪j, v⟫) :
+    t⟪k, u⟫ = t := by
+  induction t generalizing k j with
   | bvar idx =>
     simp at h
-    assumption
-  | fvar name =>
-    simp at h
-    assumption
-  | app t₁ t₂ t₁_ih t₂_ih =>
-    cases h
-    constructor
-    · apply t₁_ih
-      assumption
-    · apply t₂_ih
-      assumption
-  | tApp t T ih =>
-    cases h
-    constructor
-    · apply ih
-      assumption
-  | lam T t ih =>
-    cases h
-    constructor
-    apply ih
-    assumption
-  | tLam t ih =>
-    cases h
-    constructor
-    apply ih
-    assumption
+    by_cases idx = j
+    · aesop
+    · aesop
+  | fvar name => simp
+  | app t₁ t₂ t₁_ih t₂_ih => aesop
+  | tApp t T ih => aesop
+  | lam T t ih => aesop
+  | tLam t ih => aesop
 
-theorem lcTm_implies_lcAtTm0 {t : Tm} (h : LcTm t) : LcAtTm 0 t := by
-  induction h with
-  | fvar x => constructor
-  | app t₁ t₂ _ _ ih₁ ih₂ =>
-    constructor
-    · apply ih₁
-    · apply ih₂
-  | lam L T t _ _ ih =>
-    have ⟨x, hx⟩ := exists_fresh_name L
-    specialize ih x hx
-    constructor
-    apply lcAtTm_of_openTm
-    exact ih
-  | tApp t T _ _ ih =>
-    constructor
-    assumption
-  | tLam L t _ ih =>
-    have ⟨X, hX⟩ := exists_fresh_name L
-    specialize ih X hX
-    constructor
-    apply lcAtTm_openTmTy_inv
-    exact ih
-
-/-
-  The converse does not hold: we don't track closedness of type variables in `LcAtTm`.
--/
-theorem lcAtTm0_does_not_imply_lcTm : ∃ t, (LcAtTm 0 t ∧ ¬ LcTm t) := by
-  use ƛ (#T 0) => $v "x"
-  constructor
-  · constructor
-    constructor
-  · intro h
-    cases h with
-    | lam L T t h₁ h₂ =>
-      cases h₁
+theorem openTm_eq_id {k j : ℕ} {t v : Tm} {U : Ty} (h : t⟪j, U⟫⟪k, v⟫ = t⟪j, U⟫) :
+    t⟪k, v⟫ = t := by
+  induction t generalizing k j with aesop
 
 @[simp]
 theorem openTm_lcTm_id {u : Tm} (h : LcTm u) (k : ℕ) (v : Tm) :
     u⟪k, v⟫ = u := by
-  apply lcAtTm_openTm_id
-  · apply lcTm_implies_lcAtTm0 h
-  · simp only [zero_le]
+  induction h generalizing k v with
+  | fvar x => simp
+  | app t₁ t₂ _ _ _ _ => aesop
+  | lam L T t _ _ ih =>
+    simp only [openTm_lam, Tm.lam.injEq, true_and]
+    have ⟨x, hx⟩ := exists_fresh_name L
+    have := ih x hx (k + 1) v
+    have := @openTm_neq_id (k + 1) 0 t v ($vx) (by aesop) (by assumption)
+    rw [this]
+  | tApp t T _ _ _ => aesop
+  | tLam L t _ ih =>
+    simp only [openTm_tLam, Tm.tLam.injEq]
+    have ⟨X, hX⟩ := exists_fresh_name L
+    specialize ih X hX k v
+    have := openTm_eq_id ih
+    rw [this]
 
 theorem openTm_substTm_comm {t u v : Tm} {X : Name} {k : ℕ} (hu : LcTm u) :
     (t[X ↦ u])⟪k, v[X ↦ u]⟫ = (t⟪k, v⟫)[X ↦ u]:= by
--- SystemF/Lc/Ty.lean
+++ SystemF/Lc/Ty.lean
@@ -14,16 +14,6 @@ inductive LcTy : Ty → Prop where
   | all (L : Finset Name) T : (∀ X ∉ L, LcTy (T⟪$T X⟫)) → LcTy (∀' T)
 
 /-
-  All bound indices in `T` are strictly less than `k`.
-  This is called 'Degree' in A Charguéraud's paper.
--/
-inductive LcAtTy : ℕ → Ty → Prop where
-  | bvar i k : i < k → LcAtTy k (#T i)
-  | fvar X k : LcAtTy k ($T X)
-  | arr T₁ T₂ k : LcAtTy k T₁ → LcAtTy k T₂ → LcAtTy k (T₁ ⇒ T₂)
-  | all T k : LcAtTy (k + 1) T → LcAtTy k (∀' T)
-
-/-
   A simple locally closed type example.
 -/
 example : LcTy (∀' (#T 0 ⇒ #T 0)) := by
@@ -32,170 +22,42 @@ example : LcTy (∀' (#T 0 ⇒ #T 0)) := by
   repeat constructor
 
 
-/-
-  If `T` is locally closed at index `k`, then opening `T` at index `n ≥ k` does nothing.
--/
-theorem lcAtTy_openTy_id {T : Ty} {k : ℕ} (h : LcAtTy k T) (n : ℕ) (hn : k ≤ n) (V : Ty) :
-    T⟪n, V⟫ = T := by
-  induction h generalizing n with
-  | bvar i _ _ =>
-    have : i ≠ n := by omega
-    simp [this]
-  | fvar X k =>
-    rfl
-  | arr _ _ _ _ _ ih₁ ih₂ =>
-    simp only [openTy_arr, Ty.arr.injEq]
-    rw [ih₁, ih₂]
-    constructor
-    · rfl
-    · rfl
-    · assumption
-    assumption
-  | all T' k h' ih' =>
-    simp only [openTy_all, Ty.all.injEq]
-    have : k + 1 ≤ n + 1 := by aesop
-    rw [ih' (n + 1)]
-    assumption
-
-/-
-  If opening `T` yields a locally closed type at index `k`,
-  then `T` is locally closed at index `k + 1`.
--/
-theorem lcAtTy_of_openTy {T : Ty} {X : Name} {k : ℕ}
-    (h : LcAtTy k (T⟪k, $TX⟫)) : LcAtTy (k + 1) T := by
-  induction T generalizing k with
-  | bvar _ =>
-    simp only [openTy_bvar, beq_iff_eq] at h
-    split at h
-    · apply LcAtTy.bvar
-      aesop
-    · cases h
-      apply LcAtTy.bvar
-      omega
-  | fvar Y =>
-    apply LcAtTy.fvar
-  | arr T₁ T₂ ih₁ ih₂ =>
-    cases h
-    apply LcAtTy.arr
-    · apply ih₁; assumption
-    · apply ih₂; assumption
-  | all T' ih' =>
-    cases h
-    apply LcAtTy.all
-    apply ih'
-    assumption
-
-/-
-  If `T` is locally closed at index `k`, then it is also locally closed at any index `n ≥ k`.
--/
-lemma lcAtTy_weaken {T : Ty} {k n : ℕ} (h : LcAtTy k T) (hle : k ≤ n) :
-    LcAtTy n T := by
-  induction h generalizing n with
-  | bvar i k _ => constructor; omega
-  | fvar X k => constructor
-  | arr T₁ T₂ k _ _ ih₁ ih₂ =>
-    constructor
-    · apply ih₁; assumption
-    · apply ih₂; assumption
-  | all T k' _ ih =>
-    constructor
-    apply ih
-    omega
-
-/-
-  If `T` is locally closed at index `k + 1` and `U` is locally closed,
-  then opening `T` at index `k` with `U` yields a type that is locally closed at index `k`.
--/
-lemma lcAtTy_openTy_inv_aux {k : ℕ} {T U : Ty}
-  (hT : LcAtTy (k + 1) T) (hU : LcAtTy 0 U) : LcAtTy k (T⟪k, U⟫) := by
-  induction T generalizing k with
+theorem openTy_neq_id {k j : ℕ} {T U V : Ty} (hNeq : k ≠ j) (h : T⟪j, V⟫⟪k, U⟫ = T⟪j, V⟫) :
+    T⟪k, U⟫ = T := by
+  induction T generalizing k j with
   | bvar idx =>
-    cases hT
-    simp only [openTy_bvar, beq_iff_eq]
-    by_cases heq : idx = k
-    · simp only [heq, ↓reduceIte]
-      apply lcAtTy_weaken hU
-      omega
-    · simp only [heq, ↓reduceIte]
-      constructor
-      omega
-  | fvar name =>
-    constructor
+    simp at h
+    simp
+    by_cases idx = j
+    · aesop
+    · aesop
+  | fvar name => simp
   | arr T₁ T₂ T₁_ih T₂_ih =>
-    cases hT with
-    | arr _ _ _ ih₁ ih₂ =>
-      constructor
-      · exact T₁_ih ih₁
-      · exact T₂_ih ih₂
+    simp
+    simp at h
+    aesop
   | all T ih =>
-    cases hT with
-    | all _ _ ih' =>
-      constructor
-      apply ih
-      assumption
+    simp at h
+    simp only [openTy_all, Ty.all.injEq]
+    apply ih (j := j + 1)
+    · aesop
+    · assumption
 
-/-
-  If `T` has a bound variable, then opening `T` with a free variable yields a locally closed type.
--/
-lemma lcAtTy_openTy_inv {T : Ty} {X : Name} (h : LcAtTy 1 T) : LcAtTy 0 (T⟪$T X⟫) := by
-  apply lcAtTy_openTy_inv_aux
-  · assumption
-  · constructor
-
-/-
-  If `T` is locally closed, then `T` is locally closed at index 0
--/
-theorem lcTy_implies_lcAtTy0 {T : Ty} (h : LcTy T) : LcAtTy 0 T := by
-  induction h with
-  | fvar X =>
-    apply LcAtTy.fvar
-  | arr T₁ T₂ _ _ _ _=>
-    apply LcAtTy.arr <;> assumption
-  | all L T _ ih =>
-    apply LcAtTy.all
-    have ⟨X, hX⟩ := exists_fresh_name L
-    apply lcAtTy_of_openTy
-    exact ih X hX
-
-/-
-  If `T` is locally closed at index 0, then `T` is locally closed.
--/
-theorem lcAtTy0_implies_lcTy {T : Ty} (h : LcAtTy 0 T) : LcTy T := by
-  cases h with
-  | bvar i k _ => contradiction
-  | fvar X k => constructor
-  | arr T₁ T₂ k h₁ h₂ =>
-  constructor
-  · exact lcAtTy0_implies_lcTy h₁
-  · exact lcAtTy0_implies_lcTy h₂
-  | all T k h =>
-    apply LcTy.all ∅
-    intro X hX
-    have := lcAtTy_openTy_inv (X := X) h
-    exact lcAtTy0_implies_lcTy this
-termination_by T.size
-decreasing_by
-  all_goals (simp_all; dsimp [Ty.size]; omega)
-
-/-
-  Locally closed types are exactly those that are locally closed at index 0.
--/
-theorem lcTy_iff_lcAtTy0 {T : Ty} : LcTy T ↔ LcAtTy 0 T := by
-  constructor
-  · apply lcTy_implies_lcAtTy0
-  · intro h
-    apply lcAtTy0_implies_lcTy h
-
-/-
-  If `T` is locally closed, then opening `T` does nothing.
--/
 @[simp]
 theorem openTy_lcTy_id {U : Ty} (h : LcTy U) (k : ℕ) (V : Ty) :
     U⟪k, V⟫ = U := by
-  apply lcAtTy_openTy_id
-  · apply lcTy_implies_lcAtTy0 h
-  · simp only [zero_le]
-
+  induction h generalizing k V with
+  | fvar x => simp
+  | arr T₁ T₂ _ _ ih₁ ih₂ =>
+    simp only [openTy_arr, Ty.arr.injEq]
+    rw [ih₁, ih₂]
+    simp
+  | all L T _ ih =>
+    simp only [openTy_all, Ty.all.injEq]
+    have ⟨X, hX⟩ := exists_fresh_name L
+    have := @ih X hX (k + 1) V
+    have := @openTy_neq_id (k + 1) 0 T V ($TX) (by aesop) (by assumption)
+    rw [this]
 /-
   Substitution commutes with opening on types
 -/
